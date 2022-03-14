66d48f9eb1f556b6b21b83dbd1bd1af4a3c49b27
Maharani <maharanisakti015@gmail.com>
2022-03-14 12:04:58 +0700

    akun tutor :tambah filter di lowongan

--- src/screens/Lowongan.tsx
+++ src/screens/Lowongan.tsx
@@ -8,54 +8,170 @@ import {
   ScrollView,
   View,
 } from 'react-native';
-import {Headline, Subheading} from 'react-native-paper';
+import {Headline, Subheading, Card, Button, Text} from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
+import Modal from 'react-native-modal';
 import {AppStackParamList, MainTabParamList} from '@routes/RouteTypes';
 import {StackScreenProps} from '@react-navigation/stack';
 import {CompositeScreenProps} from '@react-navigation/native';
 import {MaterialBottomTabScreenProps} from '@react-navigation/material-bottom-tabs';
 import {apiGet} from '@utils';
+import {Picker} from '@react-native-picker/picker';
 
 type ScreenProps = CompositeScreenProps<
   MaterialBottomTabScreenProps<MainTabParamList, 'Lowongan'>,
   StackScreenProps<AppStackParamList>
 >;
-
+type FormDataType = {
+  jenjang: string;
+  prefrensi: string;
+};
 export const Lowongan: FC<ScreenProps> = ({navigation}) => {
   const [lowonganList, setLowonganList] = useState<any>([]);
+  const [filterJenjang, setFilterJenjang] = useState();
+  const [filterPrefrensi, setFilterPrenfrensi] = useState();
   const [page, setPage] = useState(1);
+  const [modalFilter, setModalFilter] = useState(false);
+  const [listJenjang, setListJenjang] = useState([]);
+  const [jenjang, setJenjang] = useState();
+  const [prefrensi, setPrefrensi] = useState();
+
   useEffect(() => {
+    let isActive = true;
     const getInitialData = async () => {
       const tutor = await apiGet({url: '/guru/profile'});
       if (tutor.data.idkecamatan != null) {
         const lowonganKabupaten = await apiGet({
-          url:
-            '/lowongan/' +
-            tutor.data.idkecamatan +
-            '?page=' +
-            page +
-            '&cari=&orderBy=idlowongan&sort=ASC',
+          url: '/lowongan/' + tutor.data.idkecamatan,
+          params: {
+            page: page,
+            cari: '',
+            orderBy: 'idlowongan',
+            sort: 'ASC',
+            jenjang: filterJenjang,
+            prefrensi: filterPrefrensi,
+          },
         });
-        setLowonganList(lowonganKabupaten.data);
+        const dataJenjang = await apiGet({url: '/paket/jenjang'});
+
+        if (isActive) {
+          setListJenjang(dataJenjang.data);
+          setLowonganList(lowonganKabupaten.data);
+        }
       } else if (tutor.data.idkecamatan == null) {
-        setLowonganList(null);
+        if (isActive) {
+          setLowonganList(null);
+        }
       }
     };
-
     getInitialData();
     return () => {
+      isActive = false;
       // cancelApiRequest();
     };
-  }, []);
+  }, [lowonganList, listJenjang, filterJenjang, filterPrefrensi, page]);
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={color.bg_grey} barStyle="dark-content" />
 
-      <Header noBackButton title="Lowongan" />
+      <Header
+        noBackButton
+        title="Lowongan"
+        withFilter={true}
+        onPressFilter={() => setModalFilter(true)}
+      />
 
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <OneLineInfo info="Klik item untuk melihat detail" />
+        <Modal
+          isVisible={modalFilter}
+          onBackdropPress={() => setModalFilter(false)}>
+          <Card
+            style={{
+              paddingVertical: 15,
+            }}>
+            <Text
+              style={{
+                fontSize: 22,
+                paddingVertical: 10,
+                textAlign: 'center',
+              }}>
+              Filter Lowongan
+            </Text>
+            <View
+              style={{
+                marginHorizontal: 20,
+                flexDirection: 'row',
+                maxHeight: 40,
+              }}>
+              {/* {' '} */}
+              <Text
+                style={{
+                  fontSize: 15,
+                  flex: 1,
+                  paddingTop: 20,
+                }}>
+                Jenjang:
+              </Text>
+              {/* TODO: BUAT DROPDOWN UNTUK JENJANG */}
+              <Picker
+                style={{flex: 2}}
+                mode="dropdown"
+                selectedValue={jenjang}
+                itemStyle={{height: 10, backgroundColor: 'white'}}
+                onValueChange={itemValue => setJenjang(itemValue)}>
+                {listJenjang.map((item: any, index: number) => {
+                  return (
+                    <Picker.Item
+                      key={index}
+                      label={item.jenjang}
+                      value={item.jenjang}
+                    />
+                  );
+                })}
+              </Picker>
+            </View>
+
+            {/* TODO: BUAT DROPDOWN UNTUK PREFRENSI */}
+            <View
+              style={{
+                marginHorizontal: 20,
+                flexDirection: 'row',
+                maxHeight: 40,
+              }}>
+              {/* {' '} */}
+              <Text
+                style={{
+                  fontSize: 15,
+                  flex: 1,
+                  paddingTop: 20,
+                }}>
+                Prefrensi:
+              </Text>
+              <Picker
+                style={{flex: 2}}
+                mode="dropdown"
+                // mode="dialog"
+                selectedValue={prefrensi}
+                itemStyle={{height: 10, backgroundColor: 'white'}}
+                onValueChange={itemValue => setPrefrensi(itemValue)}>
+                <Picker.Item label="Wanita" value="Wanita" />
+                <Picker.Item label="Pria" value="Pria" />
+                <Picker.Item label="Bebas" value="Bebas" />
+              </Picker>
+            </View>
+            <Button
+              style={{maxWidth: 100, alignSelf: 'flex-end', marginRight: 20}}
+              onPress={() => {
+                setFilterJenjang(jenjang);
+                setFilterPrenfrensi(prefrensi);
+                setModalFilter(false);
+              }}>
+              ok
+            </Button>
+          </Card>
+        </Modal>
+
         {lowonganList != null ? (
           lowonganList.map((item: any, index: number) => {
             return (
