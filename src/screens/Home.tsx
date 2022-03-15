9304ffa524be118db156ee5c5239a2dfa6a5e2c7
Firdasiska <firda.19031@mhs.unesa.ac.id>
2022-03-15 12:47:44 +0700

    warna

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -177,7 +177,7 @@ export const Home: FC<ScreenProps> = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: 'black',
     flex: 1,
     padding: dimens.standard,
   },
