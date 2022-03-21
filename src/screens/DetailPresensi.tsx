996f128288e03b6607ff8d7706c6048ec36f2ede
Maharani <maharanisakti015@gmail.com>
2022-03-21 19:52:02 +0700

    update tampilan rating dan input keterangan

--- src/screens/DetailPresensi.tsx
+++ src/screens/DetailPresensi.tsx
@@ -8,7 +8,14 @@ import {
   ScrollView,
   View,
 } from 'react-native';
-import {Card, Divider, Paragraph, Button, IconButton} from 'react-native-paper';
+import {
+  Card,
+  Divider,
+  Paragraph,
+  Button,
+  IconButton,
+  TextInput,
+} from 'react-native-paper';
 import {StackScreenProps} from '@react-navigation/stack';
 import {AppStackParamList} from '@routes/RouteTypes';
 import DatePicker from 'react-native-date-picker';
@@ -21,14 +28,55 @@ export const DetailPresensi: FC<ScreenProps> = ({navigation, route}) => {
   // const tanggalPertemuan = dayjs.unix(1630610037).format('DD MMMM YYYY');
   const tanggalPertemuan = new Date(data.tglabsen).toISOString().slice(0, 10);
   const [ratingCount, setRatingCount] = useState(0);
-  const [ratingColor, setRatingColor] = useState(undefined);
+  const [ratingColor, setRatingColor] = useState([
+    {
+      id: 1,
+      status: false,
+    },
+    {
+      id: 2,
+      status: false,
+    },
+    {
+      id: 3,
+      status: false,
+    },
+    {
+      id: 4,
+      status: false,
+    },
+    {
+      id: 5,
+      status: false,
+    },
+  ]);
   const date = new Date();
   const [Open, setOpen] = useState(false);
   const [disabledAbsen, setDisabledAbsen] = useState(false);
   const [disabledAbsenWali, setDisabledAbsenWali] = useState(false);
-
+  const [Keterangan, setKeterangan] = useState('');
+  const [hiddenButtonAbsen, setHiddenButtonAbsen] = useState(true);
+  const [inputKeterangan, setInputKeterangan] = useState(false);
   const handleRating = async (count: number) => {
+    setInputKeterangan(true);
+    setHiddenButtonAbsen(false);
     setRatingCount(count);
+    ratingColor.map(() => {
+      setRatingColor(
+        ratingColor.map((object: any) => {
+          // console.log(count);
+          if (object.id < count) {
+            return {...object, status: true};
+          } else if (object.id > count) {
+            return {...object, status: false};
+          } else if (object.id == count) {
+            return {...object, status: true};
+          }
+        }),
+      );
+    });
+
+    // setRatingColor([{id: count, status: true}]);
   };
   const {
     state: {userRole},
@@ -78,73 +126,87 @@ export const DetailPresensi: FC<ScreenProps> = ({navigation, route}) => {
             />
             <CardKeyValue keyFlex={10} keyName="Tutor" value={data.guru} />
             {/* <Paragraph style={styles.presenceStatus}>Selesai</Paragraph> */}
-            <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
-              <Button
-                disabled={
-                  userRole == 'tutor' ? disabledAbsen : disabledAbsenWali
-                }
-                style={{marginTop: dimens.standard}}
-                onPress={async () => {
-                  // if (userRole == 'parent') {
-                  const {success} = await apiPost({
-                    url: '/les/present/' + data.idabsen,
-                    payload: {keterangan: 'ada'},
-                  });
+            <Paragraph style={{fontSize: dimens.standard}}>
+              {data.flagabsenwali == 1
+                ? 'Wali sudah mengisi data hadir'
+                : 'Wali belum mengisi data hadir'}
+            </Paragraph>
 
-                  if (success) {
-                    navigation.navigate<any>('MainTabs');
-                  }
-                  // } else if (userRole == 'tutor') {
-                  //   const {success} = await apiPost({
-                  //     url: '/les/present/' + data.idabsen,
-                  //     payload: {keterangan: 'ada'},
-                  //   });
-                  //   if (success) {
-                  //     navigation.navigate<any>('MainTabs');
-                  //   }
-                  // }
-                }}>
-                {/* {userRole == 'parent' ? 'Selesai' : 'Hadir'}
-                 */}
-                hadir
-              </Button>
-              {/* {userRole == 'tutor' && ( */}
-              <Button
-                disabled={
-                  userRole == 'tutor' ? disabledAbsen : disabledAbsenWali
-                }
-                style={{marginTop: dimens.standard}}
-                onPress={async () => {
-                  const {success} = await apiPost({
-                    url: '/les/absen' + data.idabsen,
-                    payload: {keterangan: 'absen'},
-                  });
-                  if (success) {
-                    navigation.navigate<any>('MainTabs');
-                  }
-                }}>
-                Absen
-              </Button>
-              {/* )} */}
-            </View>
+            {userRole == 'parent' && hiddenButtonAbsen && (
+              <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+                <Button
+                  disabled={disabledAbsenWali}
+                  style={{marginTop: dimens.standard}}
+                  onPress={async () => {
+                    setInputKeterangan(true);
+                    setHiddenButtonAbsen(false);
+                  }}>
+                  hadir
+                </Button>
+                <Button
+                  disabled={disabledAbsenWali}
+                  style={{marginTop: dimens.standard}}
+                  onPress={async () => {
+                    const {success} = await apiPost({
+                      url: '/les/absen' + data.idabsen,
+                      payload: {keterangan: 'tidak hadir'},
+                    });
+                    if (success) {
+                      navigation.navigate<any>('MainTabs');
+                    }
+                  }}>
+                  Absen
+                </Button>
+                {/* )} */}
+              </View>
+            )}
+
             <Gap y={dimens.standard} />
-
-            {/* if not over */}
-            {/* {userRole == 'parent' && ( */}
-            {/* <> */}
             <Paragraph style={{fontSize: dimens.standard}}>
               {tanggalPertemuan}
             </Paragraph>
             <Gap y={dimens.tiny} />
             <Divider />
             <Gap y={dimens.tiny} />
-
             <CardKeyValue
               keyFlex={10}
               keyName="Les"
               value={data.paket + ' ' + data.jenjang + ' ' + data.kelas}
             />
             <CardKeyValue keyFlex={10} keyName="Tutor" value={data.guru} />
+            <Paragraph style={{fontSize: dimens.standard}}>
+              {data.flagabsen == 1
+                ? 'Tutor sudah mengisi data hadir'
+                : 'Tutor belum mengisi data hadir'}
+            </Paragraph>
+            {userRole == 'tutor' && hiddenButtonAbsen && (
+              <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+                <Button
+                  disabled={disabledAbsen}
+                  style={{marginTop: dimens.standard}}
+                  onPress={async () => {
+                    setInputKeterangan(true);
+                    setHiddenButtonAbsen(false);
+                  }}>
+                  hadir
+                </Button>
+                <Button
+                  disabled={disabledAbsen}
+                  style={{marginTop: dimens.standard}}
+                  onPress={async () => {
+                    const {success} = await apiPost({
+                      url: '/les/absen' + data.idabsen,
+                      payload: {keterangan: 'tidak hadir'},
+                    });
+                    if (success) {
+                      navigation.navigate<any>('MainTabs');
+                    }
+                  }}>
+                  Absen
+                </Button>
+                {/* )} */}
+              </View>
+            )}
             <Button
               disabled={userRole == 'tutor' ? disabledAbsen : disabledAbsenWali}
               style={{marginTop: dimens.standard}}
@@ -154,8 +216,6 @@ export const DetailPresensi: FC<ScreenProps> = ({navigation, route}) => {
               }}>
               Edit tanggal pertemuan
             </Button>
-            {/* </> */}
-            {/* )} */}
             {Open == true && (
               <DatePicker
                 modal
@@ -183,39 +243,86 @@ export const DetailPresensi: FC<ScreenProps> = ({navigation, route}) => {
             <Gap y={dimens.standard} />
 
             {/* Rating */}
-            <Card.Title
-              title="Rating Tutor"
-              titleStyle={{alignSelf: 'center'}}
-              subtitle="Rating berdasarkan kinerja tutor"
-              subtitleStyle={styles.subTitle}
-            />
-            <View style={styles.ratingContainer}>
-              <IconButton
-                icon="star"
-                size={30}
-                onPress={() => handleRating(1)}
-              />
-              <IconButton
-                icon="star"
-                size={30}
-                onPress={() => handleRating(2)}
-              />
-              <IconButton
-                icon="star"
-                size={30}
-                onPress={() => handleRating(3)}
-              />
-              <IconButton
-                icon="star"
-                size={30}
-                onPress={() => handleRating(4)}
-              />
-              <IconButton
-                icon="star"
-                size={30}
-                onPress={() => handleRating(5)}
-              />
-            </View>
+            {userRole == 'parent' && (
+              <>
+                <Card.Title
+                  title="Rating Tutor"
+                  titleStyle={{alignSelf: 'center'}}
+                  subtitle="Rating berdasarkan kinerja tutor"
+                  subtitleStyle={styles.subTitle}
+                />
+                <View style={styles.ratingContainer}>
+                  <IconButton
+                    icon="star"
+                    size={30}
+                    color={ratingColor[0].status ? 'gold' : color.grey}
+                    onPress={() => handleRating(1)}
+                  />
+                  <IconButton
+                    icon="star"
+                    size={30}
+                    color={ratingColor[1].status ? 'gold' : color.grey}
+                    onPress={() => handleRating(2)}
+                  />
+                  <IconButton
+                    icon="star"
+                    size={30}
+                    color={ratingColor[2].status ? 'gold' : color.grey}
+                    onPress={() => handleRating(3)}
+                  />
+                  <IconButton
+                    icon="star"
+                    size={30}
+                    color={ratingColor[3].status ? 'gold' : color.grey}
+                    onPress={() => handleRating(4)}
+                  />
+                  <IconButton
+                    icon="star"
+                    size={30}
+                    color={ratingColor[4].status ? 'gold' : color.grey}
+                    onPress={() => handleRating(5)}
+                  />
+                </View>
+              </>
+            )}
+            {inputKeterangan && (
+              <View>
+                <TextInput
+                  value={Keterangan}
+                  onChangeText={text => {
+                    setKeterangan(text);
+                  }}
+                  mode="outlined"
+                />
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                    width: '100%',
+                  }}>
+                  <Button
+                    onPress={async () => {
+                      setHiddenButtonAbsen(true);
+                      setInputKeterangan(false);
+                    }}>
+                    Cancel
+                  </Button>
+                  <Button
+                    onPress={async () => {
+                      console.log(Keterangan);
+                      const {success} = await apiPost({
+                        url: '/les/present/' + data.idabsen,
+                        payload: {keterangan: Keterangan},
+                      });
+                      if (success) {
+                        navigation.navigate<any>('MainTabs');
+                      }
+                    }}>
+                    Kirim
+                  </Button>
+                </View>
+              </View>
+            )}
           </Card.Content>
         </Card>
       </ScrollView>
