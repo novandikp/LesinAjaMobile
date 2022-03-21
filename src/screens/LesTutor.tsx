b967421dc59f9c4aa5d68a851a10e1f06f1b7339
Maharani <maharanisakti015@gmail.com>
2022-03-21 10:04:25 +0700

    tampilan les tutor

--- src/screens/LesTutor.tsx
+++ src/screens/LesTutor.tsx
@@ -1,5 +1,11 @@
 import React, {FC, useState, useEffect} from 'react';
-import {CardKeyValue, EmptyData, Header, OneLineInfo} from '@components';
+import {
+  CardKeyValue,
+  EmptyData,
+  Header,
+  OneLineInfo,
+  SkeletonLoading,
+} from '@components';
 import {color, dimens} from '@constants';
 import {
   SafeAreaView,
@@ -7,51 +13,33 @@ import {
   StyleSheet,
   View,
   ScrollView,
+  FlatList,
+  ListRenderItemInfo,
 } from 'react-native';
-import {Card, Chip} from 'react-native-paper';
-import dayjs from 'dayjs';
+import {Card, Chip, Button} from 'react-native-paper';
 import {CompositeScreenProps} from '@react-navigation/native';
 import {AppStackParamList, MainTabParamList} from '@routes/RouteTypes';
 import {MaterialBottomTabScreenProps} from '@react-navigation/material-bottom-tabs';
 import {StackScreenProps} from '@react-navigation/stack';
 import {apiGet} from '@utils';
+import {useIsFocused} from '@react-navigation/core';
 
 type LesType = {
-  namaLes: string;
-  totalPertemuan: number;
+  statusles: number;
+  biaya: number;
+  tglles: string;
+  kelas: string;
+  hari: string;
+  idles: number;
+  idpaket: number;
+  idortu: number;
+  jamles: string;
+  jenjang: string;
+  jumlah_pertemuan: number;
+  jeniskelamin: string;
+  paket: string;
   siswa: string;
-  pertemuanSelesai: number | null;
-  tglMulai: number | null;
-  tglSelesai: number | null;
-  tutor: string | null;
-  sudahBayar: boolean;
-  menungguTutor: boolean;
 };
-
-// const lesItems: LesType[] = [
-//   {
-//     namaLes: 'Mengaji kelas 3 SD',
-//     totalPertemuan: 8,
-//     pertemuanSelesai: 6,
-//     tglMulai: 1629698756,
-//     tglSelesai: 1631403073,
-//     siswa: 'Andi Rayka',
-//     tutor: 'Udin Harun',
-//     sudahBayar: true,
-//     menungguTutor: false,
-//   },
-//   {
-//     namaLes: 'Mengaji kelas 2 SD',
-//     totalPertemuan: 12,
-//     pertemuanSelesai: null,
-//     tglMulai: null,
-//     tglSelesai: null,
-//     siswa: 'Andi Rayka',
-//     tutor: null,
-//     sudahBayar: false,
-//     menungguTutor: false,
-//   },
-// ];
 
 type ScreenProps = CompositeScreenProps<
   MaterialBottomTabScreenProps<MainTabParamList, 'LesTutor'>,
@@ -60,57 +48,145 @@ type ScreenProps = CompositeScreenProps<
 export const LesTutor: FC<ScreenProps> = ({navigation}) => {
   const [isEmptyData, setisEmptyData] = useState(false);
   const [lesItems, setLesItem] = useState([]);
+  const [buttonLoadMore, setButtonLoadMore] = useState(true);
+  const [displayButton, setDisplayButton] = useState(false);
+  const [loadingData, setLoadingData] = useState(false);
+  const [isLoadMoreData, setLoadMoreData] = useState(false);
+  const [page, setPage] = useState(1);
+  //loading page
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const isFocus = useIsFocused();
+  const loadMoreData = async () => {
+    let NextPage = page + 1;
+    await apiGet({
+      url: '/siswa/my',
+      params: {
+        page: NextPage,
+        siswa: '',
+        orderBy: 'siswa',
+        sort: 'asc',
+      },
+    })
+      .then(res => {
+        if (res.data == null) {
+          setLoadingData(false);
+          return setDisplayButton(false);
+        }
+        setLesItem(lesItems.concat(res.data));
+        if (res.data.length < 10) {
+          console.log('its should close buton');
+          return setDisplayButton(false);
+        } else if (res.data.length == 10) {
+          setButtonLoadMore(false);
+          setPage(NextPage);
+        }
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
   useEffect(() => {
+    let isActive = true;
+
     const getInitialData = async () => {
       const siswaku = await apiGet({
         url: '/siswa/my',
+        params: {
+          page: '1',
+          siswa: '',
+          orderBy: 'siswa',
+          sort: 'asc',
+        },
       });
-      if (siswaku.data == null) {
-        setisEmptyData = true;
+      if (isActive) {
+        if (isLoadMoreData) {
+          setLesItem(lesItems);
+        }
+        if (siswaku.data == null) {
+          setisEmptyData(true);
+        } else if (siswaku.data != null) {
+          if (siswaku.data.length == 10) {
+            setButtonLoadMore(false);
+            setDisplayButton(true);
+          }
+          setLesItem(siswaku.data);
+        }
+        // setSelf(myself.data);
+        setIsLoading(false);
+        setIsRefreshing(false);
       }
-      setLesItem(siswaku.data);
-      // setLesItem(siswaku);
     };
 
-    getInitialData();
+    if (isRefreshing || isLoading || isFocus) {
+      getInitialData();
+    }
     return () => {
-      // cancelApiRequest();
+      isActive = false;
     };
   }, []);
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={color.bg_grey} barStyle="dark-content" />
-
       <Header
         noBackButton
         withFilter
         title="Daftar Les"
-        onPressFilter={() => {
-          alert('Belum jadi');
-        }}
+        // onPressFilter={() => {
+        // alert('Belum jadi');
+        // }}
       />
-
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        {isEmptyData ? (
-          <EmptyData />
-        ) : (
-          <>
-            <OneLineInfo info="Klik item untuk melihat detail" />
-
-            {lesItems.map((item, index) => {
-              return (
+      {isLoading || isRefreshing ? (
+        <SkeletonLoading />
+      ) : isEmptyData ? (
+        <EmptyData />
+      ) : (
+        <>
+          <FlatList
+            ListHeaderComponent={
+              <>
+                <OneLineInfo info="Klik item untuk melihat detail" />
+              </>
+            }
+            data={lesItems}
+            keyExtractor={(item: LesType, index: number) => index.toString()}
+            renderItem={({item}: ListRenderItemInfo<LesType>) => (
+              <>
                 <StudentItem
-                  key={index}
                   item={item}
                   onPress={() => {
                     navigation.navigate<any>('DetailLesTutor', {data: item});
                   }}
                 />
-              );
-            })}
-          </>
-        )}
-      </ScrollView>
+              </>
+            )}
+            extraData={lesItems}
+            onEndReachedThreshold={0.1}
+            ListFooterComponent={
+              <View>
+                {displayButton == true && (
+                  <Button
+                    loading={loadingData}
+                    onPress={() => {
+                      setLoadMoreData(true);
+                      setLoadingData(true);
+                      loadMoreData();
+                    }}
+                    mode="contained"
+                    disabled={buttonLoadMore}
+                    style={{
+                      marginTop: 10,
+                      alignSelf: 'center',
+                      marginHorizontal: 10,
+                    }}>
+                    Load More Data
+                  </Button>
+                )}
+              </View>
+            }
+          />
+        </>
+      )}
     </SafeAreaView>
   );
 };
@@ -119,39 +195,48 @@ const StudentItem: FC<{item: LesType; onPress: () => void}> = ({
   item,
   onPress,
 }) => {
-  const tglMulai =
-    item.tglMulai && dayjs.unix(item.tglMulai).format('DD MMMM YYYY');
-  const tglSelesai =
-    item.tglSelesai && dayjs.unix(item.tglSelesai).format('DD MMMM YYYY');
-  const jmlhPertemuanSelesai = item.pertemuanSelesai
-    ? `${item.pertemuanSelesai}/`
-    : '';
+  const tglMulai = new Date(item.tglles).toLocaleDateString();
+  const jmlhPertemuan = item.jumlah_pertemuan ? `${item.jumlah_pertemuan}` : '';
 
   const statusData = () => {
-    if (!item.tutor) {
-      return {text: 'Menunggu konfirmasi wali murid ', bgColor: '#FBBF24'};
+    if (item.statusles == 4) {
+      return {text: 'Sedang berlangsung ', bgColor: color.green_500};
     }
-
+    if (item.statusles == 3) {
+      return {
+        text: 'Sedang menunggu konfirmasi pembayaran ',
+        bgColor: color.green_500,
+      };
+    }
+    if (item.statusles == 5) {
+      return {
+        text: 'Pembayaran ditolak',
+        bgColor: color.green_500,
+      };
+    }
     return null;
   };
 
   return (
     <Card style={{marginTop: dimens.standard}} onPress={onPress}>
       <Card.Title
-        title={item.namaLes}
-        subtitle={`${jmlhPertemuanSelesai}${item.totalPertemuan} Pertemuan`}
+        title={item.paket + item.jenjang}
+        subtitle={`${jmlhPertemuan} Pertemuan`}
+        // /${item.totalPertemuan}
       />
 
       <Card.Content>
         <CardKeyValue keyName="Siswa" value={item.siswa} />
+        <CardKeyValue keyName="Tgl Mulai" value={tglMulai} />
+        <CardKeyValue keyName="Jam" value={item.jamles} />
+
         {/* {item.tutor && <CardKeyValue keyName="Tutor" value={item.tutor} />}
         {tglMulai && tglSelesai && (
           <>
-            <CardKeyValue keyName="Tgl Mulai" value={tglMulai} />
-            <CardKeyValue keyName="Tgl Selesai" value={tglSelesai} />
+            // <CardKeyValue keyName="Tgl Mulai" value={tglMulai} />
+            // <CardKeyValue keyName="Tgl Selesai" value={tglSelesai} />
           </>
         )} */}
-
         <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
           {statusData() && (
             <Chip
